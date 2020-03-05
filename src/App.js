90aea8525e8abcee7a14ac52444735ddde4daf3b
MudassirAhmedShaik <61829659+MudassirAhmedShaik@users.noreply.github.com>
2020-03-05 17:45:43 +0530

    5 Mar 2020 - 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 
-export default function App() {
+/*export default function App(){
   return (
     <div className="App">
       <h1>Hello World!!</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
+}*/
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!!</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
 }
+
+export default App;
