61b8ba77c68fb89562293eec8013c759466991cb
MudassirAhmedShaik <61829659+MudassirAhmedShaik@users.noreply.github.com>
2020-03-10 18:01:01 +0530

    10 Mar 2020- 2

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,20 +1,21 @@
 import React from "react";
 // import Radium from "radium";
 // import "./Person.css";
-import styled from "styled-components";
+// import styled from "styled-components";
+import classes from "./Person.module.css";
 
-const StyledDiv = styled.div`
-  width: 60%;
-  border: 1px solid #eee;
-  margin: 16px;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
+// const StyledDiv = styled.div`
+//   width: 60%;
+//   border: 1px solid #eee;
+//   margin: 16px;
+//   box-shadow: 0 2px 3px #ccc;
+//   padding: 16px;
+//   text-align: center;
 
-  @media (min-width: 400px) {
-    width: 350px;
-  }
-`;
+//   @media (min-width: 400px) {
+//     width: 350px;
+//   }
+// `;
 
 const person = props => {
   // const style = {
@@ -22,15 +23,16 @@ const person = props => {
   // };
 
   return (
-    <StyledDiv>
-      {/* <div className="Person" style={style}> */}
+    // <StyledDiv>
+    // <div className="Person" style={style}>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm a {props.name} and my age is {props.age} years old.
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-      {/* </div> */}
-    </StyledDiv>
+    </div>
+    // </StyledDiv>
   );
 };
 
